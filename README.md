5fdcf1b64f6936d2b945962b7b228f79195306aa
qus0in <qus0in@gmail.com>
2025-03-07 14:50:51 +0900

    servlet

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+# .env
+# https://api.together.xyz/settings/api-keys
+TOGETHER_API_KEY={}
+```
--- src/main/java/controller/ChatController.java
+++ src/main/java/controller/ChatController.java
@@ -18,11 +18,28 @@ public class ChatController extends HttpServlet {
         logger.info("안녕 나는 서블릿이야");
     }
 
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, resp);
+//        logger.info("서비스! 서비스!");
+//    }
+
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-//        super.service(req, res);
-        logger.info("서비스! 서비스!");
-        res.getWriter().println("받아라!");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8"); // 인코딩 변경
+        resp.setCharacterEncoding("UTF-8"); // 인코딩 변경
+        resp.setContentType("text/html;charset=UTF-8"); // 브라우저로 인식하는 (GET)
+        resp.getWriter().println("여기서 한글 쓰기!");
+    }
+    // 그걸로 조회를 하면 이미지를 가져다주는...
+
+    // POST -> 키워드 -> 이미지를 생성한 다음에 객체 스토리지에 저장,
+    // 해당 사진이름을 return.
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8"); // 인코딩 변경
+        resp.setCharacterEncoding("UTF-8"); // 인코딩 변경
+        resp.getWriter().println("한글을 쓰면 고장남");
     }
 
     @Override
